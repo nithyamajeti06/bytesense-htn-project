392cc8a48a95b2c251deb5efcf60e672627e223e
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-09-17 19:07:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def chatgptcall(prompt):
 
 cursor = sql.connect("Train.db")
 
+#If you want to reset the database and retrain from scratch, uncomment line 38 and then comment it again once you have created the table.
 #cursor.execute(
 #"CREATE TABLE Train(Sample string, Class string, PointID integer PRIMARY KEY)")
 
